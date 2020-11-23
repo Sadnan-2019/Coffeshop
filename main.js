cd28bb646fc2d0075caff4a1a2ab1afd96a458b1
Sadnan <sadnan.wow@gamil.com>
2020-11-23 23:00:56 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 2/12/2020.
+ */
+var typed = new Typed(".typed", {
+    strings: ["Vanila","Poller","Cone"],
+    loop:true,
+    showCursor: false,
+    typeSpeed: 70,
+    startDelay:1000
+});
